b6b1373b2724a6d2ab286784cafeb21fde71cec9
Daniel Molloy <danielmolloy_6@icloud.com>
2023-08-10 16:44:15 +0100

    twitter link added

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,17 @@
 import { animateScroll as scroll } from 'react-scroll'
+import { AiOutlineTwitter } from 'react-icons/ai'
 
 export default function Footer() {
   return (
     <div className='z-10 h-24 bottom-0 flex flex-row items-center justify-center border-t  bg-zinc-200'>
+      <div className='flex flex-col items-center justify-center'>
+      <a target="_blank" href='https://twitter.com/fizzlekelly' className=' text-2xl twitter-blue p-1 hover:text-blue-500 hover:cursor-pointer active:text-blue-400'>
+        <AiOutlineTwitter />
+      </a>
       <button className='hover:underline font-display text-sm font-light hover:cursor-pointer active:text-zinc-400' onClick={() => scroll.scrollToTop({ smooth: true, duration: 750 })}>
         Back to top
       </button>
+      </div>
     </div>
   )
 }
