8c0bdf332b1e36918082a31032c9458174c79d8a
Daniel Molloy <danielmolloy_6@icloud.com>
2024-04-29 12:15:49 +0100

    updated tests

--- __tests__/app/index.test.tsx
+++ __tests__/app/index.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
-import Home from "../../pages"
+import Home from "../../app/page"
 
 describe("<Home />", () => {
   beforeEach(() => {
